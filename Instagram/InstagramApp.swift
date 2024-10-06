bde14b97be02187ba9e77845659a38c859407b9f
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-10-05 20:19:03 -0700

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Ikbal Demirdoven on 2024-10-05.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
