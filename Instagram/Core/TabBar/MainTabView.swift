814a50a8b4b592ee6b4402e94dc2230e2f226ba2
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-10-05 22:20:18 -0700

    Set up the TabBarView

---
+++ Instagram/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  Instagram
+//
+//  Created by Ikbal Demirdoven on 2024-10-05.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
