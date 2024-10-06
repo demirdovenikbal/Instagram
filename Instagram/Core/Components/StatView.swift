d3974f03fb65bc29f618814a9123dead6c6992a6
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-10-05 23:21:24 -0700

    Created ProfileView and StatView

---
+++ Instagram/Core/Components/StatView.swift
@@ -0,0 +1,18 @@
+//
+//  StatView.swift
+//  Instagram
+//
+//  Created by Ikbal Demirdoven on 2024-10-05.
+//
+
+import SwiftUI
+
+struct StatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatView()
+}
---
+++ Instagram/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Instagram
+//
+//  Created by Ikbal Demirdoven on 2024-10-05.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
