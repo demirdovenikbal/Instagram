e4c4cd93782517f7f8cdc1ac25cf798bd11eaf65
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-10-06 20:41:45 -0700

    Implemented the FeedCell view and FeedView

---
+++ Instagram/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  Instagram
+//
+//  Created by Ikbal Demirdoven on 2024-10-06.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
