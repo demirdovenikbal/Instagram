7db237d1169fb8665e898b42ddc84514d2a76328
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2024-10-09 22:07:30 -0700

    Implemented the AddEmailView

---
+++ Instagram/Core/Authentication/AddEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  AddEmailView.swift
+//  Instagram
+//
+//  Created by Ikbal Demirdoven on 2024-10-09.
+//
+
+import SwiftUI
+
+struct AddEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddEmailView()
+}
